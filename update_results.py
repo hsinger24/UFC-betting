e27542b02be58d1cdae0b7169f07d54b79bad59d
Henry Singer <hsinger24@Henrys-MBP-2.lan>
2021-09-06 10:02:20 -0500

    Updating results for Till vs Brunson

--- update_results.py
+++ update_results.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 mma_data = pd.read_csv('mma_data.csv', index_col = 0)
-print(mma_data)
-# results = [0,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,-1,1,1,1,1,-1,1,0,0,0,-1,-1,0,-1,0,0,0,1,-1,0,1]
-# print(len(results) == len(mma_data))
+#print(mma_data)
+results = [0,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,-1,1,1,1,1,-1,1,0,0,0,-1,-1,0,-1,0,0,0,1,-1,0,1,1,1,1,0,-1,-1,-1,1,0,-1]
+print(len(results) == len(mma_data))
 
